a73486246ba94760eb1204019bb5d62f36e58a01
Todd Wickizer <you@example.com>
2020-08-10 09:34:56 -0700

    add antigen testing

---
+++ antigen/precache-manifest.7c9d01a00870217f62579388805fc144.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4f1a328d4e75831807c96b0fb96a5f6",
+    "url": "/static/media/t3logo.a4f1a328.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c9c319bedc925fd73419",
+    "url": "/static/js/main.c9c319be.chunk.js"
+  },
+  {
+    "revision": "3324209213f7697232d8",
+    "url": "/static/js/1.33242092.chunk.js"
+  },
+  {
+    "revision": "c9c319bedc925fd73419",
+    "url": "/static/css/main.5896640d.chunk.css"
+  },
+  {
+    "revision": "3324209213f7697232d8",
+    "url": "/static/css/1.1101dc28.chunk.css"
+  },
+  {
+    "revision": "02fe5298772c0e77953950bace34eb64",
+    "url": "/index.html"
+  }
+];
